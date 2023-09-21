fc05e6461e024ab60d7208935975f4babbdd00ca
aabir <aabir@Mac-Pro.local>
2023-09-21 20:14:50 +0600

    My Neovim config

---
+++ lua/plugins.lua
@@ -0,0 +1,188 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim --
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+    --color
+    use {
+        "folke/tokyonight.nvim",
+      }
+    -- Common utilities
+    use("nvim-lua/plenary.nvim")
+
+    -- Icons
+    use("nvim-tree/nvim-web-devicons")
+
+    -- Statusline
+    use({
+        "nvim-lualine/lualine.nvim",
+        event = "BufEnter",
+        config = function()
+            require("configs.lualine")
+        end,
+        requires = { "nvim-web-devicons" },
+    })
+
+    -- Treesitter
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            require("nvim-treesitter.install").update({ with_sync = true })
+        end,
+        config = function()
+            require("configs.treesitter")
+        end,
+    })
+
+    use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
+
+    -- Telescope
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
+
+    -- LSP
+    use({
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("configs.lsp")
+        end,
+    })
+
+    use("onsails/lspkind-nvim")
+    use({
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        tag = "v<CurrentMajor>.*",
+    })
+
+    -- cmp: Autocomplete
+    use({
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        config = function()
+            require("configs.cmp")
+        end,
+    })
+
+    use("hrsh7th/cmp-nvim-lsp")
+
+    use({ "hrsh7th/cmp-path", after = "nvim-cmp" })
+
+    use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
+
+    -- LSP diagnostics, code actions, and more via Lua.
+    use({
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+            require("configs.null-ls")
+        end,
+        requires = { "nvim-lua/plenary.nvim" },
+    })
+
+    -- Mason: Portable package manager
+    use({
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    })
+
+    use({
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("configs.mason-lsp")
+        end,
+    })
+
+    -- File manager
+    use({
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+    })
+
+    -- Show colors
+    use({
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "*" })
+        end,
+    })
+
+    -- Terminal
+    use({
+        "akinsho/toggleterm.nvim",
+        tag = "*",
+        config = function()
+            require("configs.toggleterm")
+        end,
+    })
+
+    -- Git
+    use({
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("configs.gitsigns")
+        end,
+    })
+
+    -- Markdown Preview
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    })
+
+    -- Auto pairs
+    use({
+        "windwp/nvim-autopairs",
+        config = function()
+            require("configs.autopairs")
+        end,
+    })
+
+    -- Background Transparent
+    use({
+        "xiyaowong/nvim-transparent",
+        config = function()
+            require("transparent").setup({
+                
+                extra_groups = {
+                    "BufferLineTabClose",
+                    "BufferlineBufferSelected",
+                    "BufferLineFill",
+                    "BufferLineBackground",
+                    "BufferLineSeparator",
+                    "BufferLineIndicatorSelected",
+                },
+                exclude_groups = {},
+            })
+        end,
+    })
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
